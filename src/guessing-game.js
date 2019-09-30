1cf4a9dfa1e86f082d65f1d62fe5a8a76dce7b94
VukZh <vukfx@inbox.ru>
2019-09-30 19:20:53 +0600

    change guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,30 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.mn = 0;
+    this.mx = 0;
+    this.currFundNum = 0;
+  }
+  setRange(mn, mx) {
+    this.mn = mn;
+    this.mx = mx;
+    console.log("min " + this.mn + " max " + this.mx);
+  }
 
-    setRange(min, max) {
+  guess() {
+    this.currFundNum = Math.round((this.mx - this.mn) / 2) + this.mn;
+    // console.log("min " +  this.mn + " max " + this.mx + " --currFundNum-- " + this.currFundNum);
+    return this.currFundNum;
+  }
 
-    }
+  lower() {
+    this.mx = this.currFundNum;
+    // console.log(this.mn + " ----------------- " + this.mx);
+  }
 
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.mn = this.currFundNum;
+    // console.log(this.mn + " ----------------- " + this.mx);
+  }
 }
 
 module.exports = GuessingGame;
